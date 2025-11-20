5bf923f602de78c349c6d352e5d02face2ce5b45
vinny <viniciusrafael2435@gmail.com>
2025-11-20 23:06:02 +0000

    Arrumando repositorio

--- src/main/java/com/EcoTech/demo/InnovatonSiteApplication.java
+++ src/main/java/com/EcoTech/demo/InnovatonSiteApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class InnovatonSiteApplication {
+    //toma
 
 	public static void main(String[] args) {
 		SpringApplication.run(InnovatonSiteApplication.class, args);
